d9725e4c3fb0125617a89b5e3c6302a7c9fd1b09
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-04-02 10:50:03 +0530

    Render basic tic-tac-toe grid

---
+++ src/board.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+        </div>
+        <div className="board-row">
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+        </div>
+        <div className="board-row">
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
