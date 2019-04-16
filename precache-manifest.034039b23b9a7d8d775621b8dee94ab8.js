ed9de4f27c70ae09f47ab31cffa04d2f01f4e98f
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-04-16 17:20:50 +0530

    Updates

---
+++ precache-manifest.034039b23b9a7d8d775621b8dee94ab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "f94d962a5e6d11d9e839",
+    "url": "/tic-tac-toe/static/js/main.0dace2cb.chunk.js"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/tic-tac-toe/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "f94d962a5e6d11d9e839",
+    "url": "/tic-tac-toe/static/css/main.6bcd91e3.chunk.css"
+  },
+  {
+    "revision": "7f5fd3064597e066811ab639361b1b38",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
